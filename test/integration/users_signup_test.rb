644ac4fabf4cc8498e20ea3aa0fd54327216434b
okada <kirinnremonn1122@gmail.com>
2018-08-26 22:22:47 +0900

    fix test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,15 +2,26 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
-  test "invalid signup informaion" do
+test "invalid signup information" do
+  get signup_path
+  assert_no_difference 'User.count' do
+  post users_path, params: { user: { name:  "",
+    email: "user@invalid",
+    password:              "foo",
+    password_confirmation: "bar" } }
+    end
+  assert_template 'users/new'
+end
+
+  test "valid signup informaion" do
     get signup_path
     assert_difference 'User.count', 1 do
-      post users_path, params: { user: { name: "",
-                          email: "user@invalid",
-                          password:         "foo",
-                          password_confirmation: "bar"} }
+      post users_path, params: { user: { name: "Example User",
+                          email: "user@example.com",
+                          password:         "password",
+                          password_confirmation: "password"} }
     end
-    follow_redirect
-    assert_select 'users/show'
+    follow_redirect!
+    assert_template 'users/show'
   end
 end
